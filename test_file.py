44b38ae030c8c9e0ed6fc81f13d8cc8865834263
masbulhaider <masbul.haider@infinitibit.com>
2025-10-29 12:28:15 +0600

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -5,7 +5,7 @@ def calculate_sum(a, b):
 def main()
     x = 6/
     y = 10
-    total = calculate_sum(x, u)//
+    total = calculate_sum(x, u)/
     print(f"Sum: {total})/
 if __name__ == "__main__":
  calculate(3,4)
